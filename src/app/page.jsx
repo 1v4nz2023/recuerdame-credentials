d911bc41e2c31926bbad9bae198af3e31f48c59d
1v4nz2023 <129082143+1v4nz2023@users.noreply.github.com>
2024-11-05 19:12:10 -0500

    19:11

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <main className="h-[calc(100vh-7rem)] flex justify-center items-center mx-auto py-20">
       <section className="grid grid-cols-2 gap-4 m-2">
-        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-1 row-span-1 justify-center items-center">
+        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-1 row-span-1 justify-center items-center hover:bg-secondary">
         <span className="text-[1rem] md:text-[2rem] text-white py-2">
             Descarga la app
           </span>
@@ -19,7 +19,7 @@ const Homepage = () => {
             />
           </Link>       
         </div>
-        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-1 row-span-1 justify-center items-center">
+        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-1 row-span-1 justify-center items-center  hover:bg-secondary" >
         <span className="text-[1rem] md:text-[2rem] text-white py-2">
            ¿Necesitas ayuda?
           </span>
@@ -35,7 +35,7 @@ const Homepage = () => {
           </Link>
         </div>
 
-        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-2 row-span-1">
+        <div className="p-6 border  bg-babyblue border-babyblue rounded-[12px] flex flex-col gap-1 col-span-2 row-span-1  hover:bg-secondary">
         <span className="text-[1rem] md:text-[2rem] text-white py-2">
             Servicios
           </span>
