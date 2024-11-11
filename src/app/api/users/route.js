526511413aa6eac5662898447cc4334b12d15ad3
1v4nz2023 <129082143+1v4nz2023@users.noreply.github.com>
2024-11-11 16:09:26 -0500

    vf12

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,13 +1,18 @@
+const { NextResponse } = require("next/server");
+import db from "@/libs/db";
+
 export async function GET() {
   try {
     const users = await db.user.findMany();
-    const response = NextResponse.json(users);
-    response.headers.set("Access-Control-Allow-Origin", "*"); // Permitir solicitudes de todos los orígenes
-    return response;
+    return NextResponse.json(users);
   } catch (error) {
     return NextResponse.json(
-      { message: error.message },
-      { status: 500 }
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
     );
   }
 }
