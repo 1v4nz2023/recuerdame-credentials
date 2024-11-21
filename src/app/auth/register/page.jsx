c7c2c2561b4283c08f82d016e5e61b88e6189118
1v4nz2023 <129082143+1v4nz2023@users.noreply.github.com>
2024-11-21 03:04:27 -0500

    21-11

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,17 +1,71 @@
 "use client";
-
+import { FaSearch } from "react-icons/fa";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 function RegisterPage() {
   const {
     register,
     handleSubmit,
+    setValue,  
+    getValues, 
     formState: { errors },
   } = useForm();
   const router = useRouter();
 
+  // Función para consultar el DNI
+  const consultarDni = async () => {
+    const dni = getValues("username"); 
+    if (!dni || dni.length != 8) {
+      return Swal.fire({
+        title: 'Error',
+        text: 'Debe ingresar un DNI válido',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+    }
+
+    try {
+      const res = await fetch("https://api.consultasperu.com/api/v1/query", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: "f2b4b6603440b08ccf574a20e857127c6f9be1b1e048a59add63e145c9b0201e", 
+          type_document: "dni",
+          document_number: dni,
+        }),
+      });
+
+      const responseData = await res.json();
+
+      if (res.ok && responseData.success) {
+        const data = responseData.data;
+        setValue("nombres", `${data.full_name}`);
+        setValue("email", `${data.number}@gmail.com`);
+
+      } else {
+        Swal.fire({
+          title: 'Error',
+          text: responseData.message || 'No se encontraron datos para este DNI',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    } catch (error) {
+      console.error("Error al consultar el DNI:", error);
+      Swal.fire({
+        title: 'Error',
+        text: 'Hubo un problema al consultar los datos del DNI',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
+    }
+  };
+
+  // Función para manejar el envío del formulario
   const onSubmit = handleSubmit(async (data) => {
     if (data.password !== data.confirmPassword) {
       return Swal.fire({
@@ -36,35 +90,63 @@ function RegisterPage() {
         "Content-Type": "application/json",
       },
     });
+
+    const responseData = await res.json(); 
+
     if (res.ok) {
-      router.push("/auth/login");
+      Swal.fire({
+        title: 'Registro exitoso',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router.push("/dashboard");
+        }
+      });
+    } else {
+      Swal.fire({
+        title: 'Error',
+        text: responseData.message,
+        icon: 'error',
+        confirmButtonText: 'OK'
+      });
     }
-    console.log(res);
   });
 
-  console.log(errors);
+
   return (
     <div className="h-[100vh] flex justify-center items-center sm:my-10 my-20">
       <form onSubmit={onSubmit} className="w-1/2 xl:w-1/4">
         <h1 className=" text-primary font-bold text-2xl xl:text-4xl mb-4">Registrar</h1>
+        
         <label htmlFor="username" className="text-slate-500 mb-2 block text-sm">
           DNI de usuario:
         </label>
-        <input
-          type="text"
-          {...register("username", {
-            required: {
-              value: true,
-              message: "El dni de usuario es requerido",
-            },
-          })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-white w-full"
-          placeholder="DNI"
-        />
+        <div className="flex">
+          <input
+            type="number"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "El dni de usuario es requerido",
+              },
+              pattern: {
+                value: /^[0-9]{8}$/,
+                message: "El DNI debe tener 8 dígitos",
+              },
+            })}
+            className="p-3 rounded-l block mb-2 bg-slate-900 text-white w-full"
+            placeholder="DNI"
+          />
+          <button
+            type="button"
+            onClick={consultarDni}
+          >
+            <FaSearch className="text-white text-[3rem] bg-gray-400 hover:bg-gray-500 px-2 mb-2 rounded-r" />
+          </button>
+        </div>
         {errors.username && (
-          <span className="text-red-500 text-sm">
-            {errors.username.message}
-          </span>
+          <span className="text-red-500 text-sm">{errors.username.message}</span>
         )}
 
         <label htmlFor="nombres" className="text-slate-500 mb-2 block text-sm">
@@ -78,23 +160,28 @@ function RegisterPage() {
               message: "Los nombres de usuario son requeridos",
             },
           })}
-          className="p-3 rounded block mb-2 bg-slate-900 text-white w-full"
+          className="p-3 rounded block mb-2 bg-slate-900 text-white w-full "
           placeholder="Nombres"
+          disabled
+
         />
-        {errors.username && (
+        {errors.nombres && (
           <span className="text-red-500 text-sm">{errors.nombres.message}</span>
         )}
 
         <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">
           Correo:
         </label>
-
         <input
           type="email"
           {...register("email", {
             required: {
               value: true,
               message: "Por favor ingrese un correo válido",
+            },
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Por favor ingrese un correo electrónico válido",
             },
           })}
           className="p-3 rounded block mb-2 bg-slate-900 text-white w-full"
@@ -103,10 +190,10 @@ function RegisterPage() {
         {errors.email && (
           <span className="text-red-500 text-sm">{errors.email.message}</span>
         )}
+
         <label htmlFor="password" className="text-slate-500 mb-2 block text-sm">
           Contraseña:
         </label>
-
         <input
           type="password"
           {...register("password", {
@@ -119,14 +206,10 @@ function RegisterPage() {
           placeholder="********"
         />
         {errors.password && (
-          <span className="text-red-500 text-sm">
-            {errors.password.message}
-          </span>
+          <span className="text-red-500 text-sm">{errors.password.message}</span>
         )}
-        <label
-          htmlFor="confirmPassword"
-          className="text-slate-500 mb-2 block text-sm"
-        >
+
+        <label htmlFor="confirmPassword" className="text-slate-500 mb-2 block text-sm">
           Confirma la contraseña:
         </label>
         <input
@@ -141,10 +224,9 @@ function RegisterPage() {
           placeholder="********"
         />
         {errors.confirmPassword && (
-          <span className="text-red-500 text-sm">
-            {errors.confirmPassword.message}
-          </span>
+          <span className="text-red-500 text-sm">{errors.confirmPassword.message}</span>
         )}
+
         <button className="w-full bg-babyblue text-white p-3 rounded-lg mt-2 hover:bg-sky-700">
           Registrar
         </button>
